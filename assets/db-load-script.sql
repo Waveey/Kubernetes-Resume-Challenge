c7c805bf2c41e1353592e62d5759b42a51f2ad13
Waveey <ogonnannamani11@gmail.com>
2024-03-23 04:45:25 +0100

    update Dockerfile

--- assets/db-load-script.sql
+++ assets/db-load-script.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE ecomdb;
 USE ecomdb;
 CREATE TABLE products (id mediumint(8) unsigned NOT NULL auto_increment,Name varchar(255) default NULL,Price varchar(255) default NULL, ImageUrl varchar(255) default NULL,PRIMARY KEY (id)) AUTO_INCREMENT=1;
 
